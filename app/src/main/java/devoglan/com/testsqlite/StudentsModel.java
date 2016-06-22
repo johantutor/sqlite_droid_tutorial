bfdf20c5bb9185509349fba611dc18f4210fa229
jhngolan <joparsip@gmail.com>
2016-06-22 15:51:14 +0700

    Rev1

---
+++ app/src/main/java/devoglan/com/testsqlite/StudentsModel.java
@@ -0,0 +1,18 @@
+package devoglan.com.testsqlite;
+
+public class StudentsModel {
+
+    public int id;
+    public String name;
+    public String phone_number;
+
+    public StudentsModel(int id, String name, String phone_number) {
+        // TODO Auto-generated constructor stub
+        this.id = id;
+        this.name = name;
+        this.phone_number = phone_number;
+    }
+    public StudentsModel(){
+
+    }
+}
